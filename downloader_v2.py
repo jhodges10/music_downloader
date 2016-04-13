49d19b4f26505a5630812c40e7de7f89c03cfbed
jhodges10 <jeffhodges93@gmail.com>
2016-04-12 19:57:36 -0700

    Cleaned up some stuff
    - made it download to 'downloads' directory one directory up, you might
    have to add it in case it doesn't exist
    - fixed the beatifulsoup confusion error regarding xml/html
    - removed a redundant variable
    - added eyeD3 for ID3 tags but it doesn't work so it's commented out
    - it now tells you which video it's going to try and download!

--- downloader_v2.py
+++ downloader_v2.py
@@ -6,7 +6,9 @@ import urllib #This submits the html for youtube
 import urllib2 #This downloads the HTML and parses it
 from bs4 import BeautifulSoup #This does the hard work on the HTML
 import pafy #This does Youtube API stuff
+#import eyeD3 #This does ID3 tags for mp3s
 import time
+
 
 ## You have to pip install youtube-dl as well for pafy to work right
 
@@ -24,28 +26,29 @@ with open('sample.csv') as csvfile:
 y= 0
 
 for each in videos:
-    tempstring = ','.join(videos[y]) #convert list to string
-    searchterm = tempstring #make nice variable name
-    print searchterm #print out which one we're searching
+    searchterm = ','.join(videos[y]) #convert list to string
+    print "This is what we're looking for: " +searchterm #print out which one we're searching
     y+=1 #increase the position in the list
     query = urllib.quote(searchterm) #define the query string
     url = "https://www.youtube.com/results?search_query=" +query #generate url query
     response = urllib2.urlopen(url) #download the response
     html = response.read() #read that HTML wassup
-    soup = BeautifulSoup(html) #make some soup out of that HTML
+    soup = BeautifulSoup(html, "lxml") #make some soup out of that HTML
+
     # LETS LOOP SOME SHIT
     for vid in soup.findAll(attrs={'class':'yt-uix-tile-link'}): #goes through every line that it pulls out which is a youtube video link (including friggen playlists)
         url = 'https://www.youtube.com' + vid['href'] #generate the nice URL
         video = pafy.new(url) #turn that into something that our youtube API tool can use
         vid_title = video.title #grab the title for it
         #attempt to only grab ones that don't contain the wrong title
+        print vid_title
         if "remix" not in vid_title:
             video_audio = video.getbestaudio()
         elif "cover" not in vid_title:
             video_audio = video.getbestaudio()
         #deal with weird bug in pafy where it has problems with files already existing since the temp name is bad
         try:
-            filename = video_audio.download(quiet=True)
+            filename = video_audio.download(filepath="../downloads/", quiet=True)
         except:
             pass
         #somewhat redundant?
